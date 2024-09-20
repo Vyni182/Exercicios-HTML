3055fb305dffcc0f4edcfd21cafa9b7b816d9a63
Vinicius Silva <vyniciush@gmail.com>
2024-09-20 01:44:59 -0300

    Add files via upload

---
+++ scriptBotao.js
@@ -0,0 +1,30 @@
+let botao = document.querySelector("#botao");
+botao.style.background="white";
+let estaQuebrado=false;
+let contaCliques=0;
+
+botao.addEventListener("mouseover", e =>{
+    if(!estaQuebrado){
+         botao.style.background="green";
+         botao.style.color="white";
+    }
+});
+
+botao.addEventListener("mouseout", e =>{
+    if(!estaQuebrado){
+          botao.style.background="blue";
+    }
+});
+
+botao.addEventListener("click", e =>{
+
+    contaCliques++;
+
+    if(contaCliques>=10){
+        botao.style.background="red";
+        botao.innerHTML="quebrei";
+        estaQuebrado=true;
+    }
+
+});
+
